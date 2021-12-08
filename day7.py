a7fca815f562c5396a93866b840ea1a634a15678
Emily Hough-Kovacs <emilyhoughkovacs@gmail.com>
2021-12-07 16:19:34 -0800

    day 7 parts 1 and 2

---
+++ day7.py
@@ -0,0 +1,41 @@
+import sys
+import os
+
+class Solution(object):
+    def __init__(self):
+        self.args = [arg for arg in sys.argv[1:]]
+
+    def solution(self, args):
+
+        # uses first command line arg if exists else inputfile.txt
+        file = args[0] if args and os.path.isfile(args[0]) else 'inputday7.txt'
+        f = open(file, 'r')
+        line = f.read().split(',')
+        f.close()
+
+        line = [int(x) for x in line]
+
+        line = sorted(line)
+
+
+        def distance(line, pos):
+            distance = 0
+            for x in line:
+                distance+=abs(x-pos)
+            return distance
+
+        minDistance = distance(line, line[0])
+
+        for p in range(line[0]+1, line[-1]+1):
+            dist = distance(line, p)
+            if dist < minDistance:
+                minDistance=dist
+
+        return minDistance        
+
+def main():
+    s = Solution()
+    print(s.solution(s.args))
+
+if __name__ == '__main__':
+    main()
