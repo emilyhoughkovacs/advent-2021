8e05e7f54f941ae5c12856fd6d8413fb846fd4de
Emily Hough-Kovacs <emilyhoughkovacs@gmail.com>
2021-12-14 12:12:26 -0800

    day 14 part 1

---
+++ day14.py
@@ -0,0 +1,50 @@
+import sys
+import os
+from collections import Counter
+
+class Solution(object):
+    def __init__(self):
+        self.args = [arg for arg in sys.argv[1:]]
+
+    def solution(self, args):
+
+        # uses first command line arg if exists else inputfile.txt
+        file = args[0] if args and os.path.isfile(args[0]) else 'inputday14.txt'
+        f = open(file, 'r')
+        lines = f.read().splitlines()
+        f.close()
+
+        polymer_template = lines[0]
+
+        global pair_insertion
+        pair_insertion = {line.split(" -> ")[0]:line.split(" -> ")[1] for line in lines[2:]}
+
+        def polymerize(curString=polymer_template):
+            newString = ''
+            for i in range(len(curString)-1):
+                segment = curString[i:i+2]
+                if segment in pair_insertion.keys():
+                    segment = curString[i]+pair_insertion[curString[i:i+2]]+curString[i+1]
+                newString+=segment[0:2]
+            if len(segment)==3:
+                newString+=segment[2]
+
+            return newString
+
+
+        blah = polymerize()
+        for i in range(2, 11):
+            blah = polymerize(blah)
+            print(i, blah)
+
+        counter = Counter(blah)
+        print(counter)
+
+        return pair_insertion
+
+def main():
+    s = Solution()
+    print(s.solution(s.args))
+
+if __name__ == '__main__':
+    main()
